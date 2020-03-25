4013696cdf69d77daa15ace4bdc5ae9d652c395d
toniochando <42381995+toniochando@users.noreply.github.com>
2020-03-25 19:02:46 +0100

    1

--- src/app.js
+++ src/app.js
@@ -19,6 +19,10 @@ app.set('view engine','ejs');
 */
 app.use(express.static(path.join(__dirname, '/public')));
 
+/*In app.js near your other app.use statement add express middleware to handle POST data. With the use function add the express.urlencoded middleware to app. Make sure to set the extended option to true.
+*/
+app.use(express.urlencoded({ extended: true }));
+
 /*In app.js above the index route, use the readFileSync function of the built-in fs library to read the contents of the file located at src/json/accounts.json. Note: read the file with the UTF8 encoding.
 
 Declare a const called accountData to store the contents of the file. accountData now contains JSON, use JSON.parse to convert it to a javascript object.
@@ -57,6 +61,64 @@ Now that you have created the savings account route, create similar routes for t
 app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
 app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
 app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+
+/*Near your other routes in app.js create a get route that points to the /transfer URL path. It should render the transfer view
+*/
+app.get('/transfer', (req, res) =>  res.render('transfer'));
+
+/*
+Switch back to app.js and create a post route that points to the /transfer URL path. We will fill in the body of the function for this route in the next few steps.
+*/
+app.post('/transfer', (req, res) => {
+
+    /*Still in app.js and in the function body of the post route we are going to calculate the new balances for the account we are transferring from.
+    We have several values that have been entered into the HTML form in transfer.ejs. Upon form submission the request body will contain from, to, and amount. Current account balances are stored in the accounts object.
+    As an example to access the current balance for the savings account use accounts["savings"].balance. Using these values, calculate the new balance of the account we are transferring from. Then set the balance to that amount.
+    */
+    accounts[req.body.from].balance -= req.body.amount;
+    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
+
+    /*Still in app.js and in the function body of the post route, convert the accounts javascript object to a string using the JSON.stringify function save this string in a variable called accountsJSON.
+    */   
+    let accountsJSON = JSON.stringify(accounts, null, 4)
+
+    /*Still in app.js and in the function body of the post route, use the writeFileSync function of the built-in fs library to write the variable accountsJSON to the file located at json/accounts.json.
+
+    Notes:
+    If at any point accounts.json gets overwritten and you would like the original back copy the JSON from account_backup.json to account.json
+    Specify the absolute file path using path.join & __dirname
+    Write the file with the UTF8 encoding.
+    */
+    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    /*Still in app.js and in the function body of the post route, render the transfer view and pass an object with the following key value pair:
+    message: "Transfer Completed"
+    */
+    res.render('transfer', {message: 'Transfer Completed'});
+});
+
+/*The payment feature of the application is similar to the transfer feature. Add this new feature using this general outline of the steps:
+
+    Near your existing routes in app.js create a get route with a URL path of /payment that renders the payment view and passes in an object with a key-value pair of account: accounts.credit.
+
+    Below the payment get route create a post route with a URL path of /payment and an empty function.
+
+    In the body of the payment post route function:
+        Subtract req.body.amount from accounts.credit.balance and save it back to accounts.credit.balance
+        Add req.body.amount to accounts.credit.available and save it back to accounts.credit.available remember to use parseInt() on both values when adding.
+        Convert the accounts javascript object to a JSON string and save it to a variable called accountsJSON
+        Write accountsJSON to a file. Note: write the file with the UTF8 encoding.
+        Render the payment view and pass an object with the following key value pairs, { message: "Payment Successful", account: accounts.credit }
+*/
+app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount);
+    let accountsJSON = JSON.stringify(accounts, null, 4)
+    fs.writeFileSync(path.join(__dirname, 'json','accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
+});
+
+
 
 /*In app.js below the account get routes create a get route that points at the /profile URL path. Render the profile view and pass an object with the following key value pair:
 
