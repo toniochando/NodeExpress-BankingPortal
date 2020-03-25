07380a6217089ba23db6320137a96467aa9e500b
toniochando <42381995+toniochando@users.noreply.github.com>
2020-03-25 16:58:07 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,9 +9,12 @@ app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-/*const accountData = fs.readFileSync (
-    path.join(__dirname, 'json', 'account.json'), 'utf8'
-);*/
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData);
+
 
 app.get('/', (req, res) => res.render('index', { title: 'Index' }));
 
