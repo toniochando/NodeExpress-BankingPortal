129ea4e548fa420a6a08672f045f3cf59082f6b1
toniochando <42381995+toniochando@users.noreply.github.com>
2020-03-25 16:44:50 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,9 +9,7 @@ app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-/*const accountData = fs.readFileSync (
-    path.join(__dirname, 'json', 'account.json'), 'utf8'
-);*/
+
 app.get('/', (req, res) => res.render('index', { title: 'Index' }));
 
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
