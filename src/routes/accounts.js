cbd7f6decb2bc8f20f812adcfee555c964056487
toniochando <42381995+toniochando@users.noreply.github.com>
2020-03-26 12:24:11 +0100

    Update accounts.js

---
+++ src/routes/accounts.js
@@ -0,0 +1,6 @@
+/*In the new accounts.js require the express framework and store a reference to it in a const called express. Next, call the express.Router() function and store it in a const called router.
+Error
+Has the express framework been required in src/routes/accounts.js?
+*/
+const express = require('express');
+const router = express.Router();
