2b1608669b105b8b43c34f89298c9c44fc1b42ca
toniochando <42381995+toniochando@users.noreply.github.com>
2020-03-26 12:12:07 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -4,5 +4,12 @@
 const fs = require('fs');
 const path = require('path');
 
+/*In app.js locate the lines that are responsible for reading and parsing JSON from the src/json/accounts.json file. Copy and paste them to the new data.js file below the require statements.
+*/
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
+
+/*In app.js locate the lines that are responsible for reading and parsing JSON from the src/json/users.json file. Copy and paste them to the new data.js file below the accounts const.
+*/
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData)
