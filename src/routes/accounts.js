5ef489c3b69e07b428b8edf6cfe122943dfbb02d
toniochando <42381995+toniochando@users.noreply.github.com>
2020-03-26 12:25:19 +0100

    Update accounts.js

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -4,3 +4,10 @@ Has the express framework been required in src/routes/accounts.js?
 */
 const express = require('express');
 const router = express.Router();
+
+/*In accounts.js require data.js and at the same time use object destructuring to create one constant called accounts. Note: use relative paths to require the data module. Use '../data' since it is one level up.
+Error
+
+Has data.js been required and the accounts const been created in src/routes/accounts.js?
+*/
+const { accounts } = require('../data.js');
