4b1d5c0c2ffdd3c1ccd99f9fd43ffa2aa2bd8a88
toniochando <42381995+toniochando@users.noreply.github.com>
2020-03-26 12:32:24 +0100

    Update services.js

---
+++ src/routes/services.js
@@ -0,0 +1,10 @@
+/*Require Express
+
+In the new services.js require the express framework and store a reference to it in a const called express. Next, call the express.Router() function and store it in a const called router.
+Error
+
+Has the express framework been required in src/routes/services.js?
+*/
+const express = require('express');
+
+const router = express.Router();
